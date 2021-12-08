cf1f15bffac7a3fe482a734033fa11a4a21a27d2
Konstantin Beloglazov <ajoq@ya.ru>
2021-12-08 12:14:14 +0300

    dev v2

---
+++ src/js/__tests__/tests.js
@@ -0,0 +1,11 @@
+import read from '../reader';
+import json from '../parser';
+import GameSavingLoader from '../app';
+
+test('load return promise', () => {
+    const expected = new Promise((resolve, reject) => {
+        resolve('{"id":9,"created":1546300800,"userInfo":{"id":1,name":"Hitman","level":10,"points":2000}}');
+    });
+    const received = new GameSavingLoader();
+    expect(received.load()).toEqual(expected);
+  });
--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,59 @@
 import json from './parser';
 import read from './reader';
-import GameSaving from './gameSaving';
 
 export default class GameSavingLoader {
     load() {
-        console.log('Привет')
+        return new Promise((resolve, reject) => {
+            read()
+            .then((result) => {
+                return json(result);
+            })
+            .then((result) => {
+                resolve(result);
+            })
+        })
     }
 }
 
-const test = new GameSavingLoader();
-test.load();
+// GameSavingLoader.load().then((saving) => {
+//     // saving объект класса GameSaving
+//     console.log(saving);
+//   }, (error) => {
+//     // ...
+//   });
+
+
+// const test = new GameSavingLoader();
+// console.log(test.load());
+// test.load().then((saving) => {
+//     console.log(saving);
+//     return saving;
+//   }, (error) => {
+//       console.log(error);
+//   });
+
+// let p = new Promise((resolve, reject) => {
+//     resolve('{"id":9,"created":1546300800,"userInfo":{"id":1,name":"Hitman","level":10,"points":2000}}');
+// });
+// console.log(p);
+
+// const res1 = json(read1);
+// console.log(res1);
+
+// read1.then((result) => {
+//     console.log(result);
+//     const decodeBuf = new Uint16Array(result);
+//     console.log(decodeBuf);
+//     let resultArr2 = String.fromCharCode.apply(null, decodeBuf)
+//     console.log(resultArr2);
+//     // let resultArr = '';
+//     // decodeBuf.forEach((item) => {
+//     //     resultArr += String.fromCharCode(item);
+//     // });
+//     // console.log(resultArr);
+// });
+
+// res1.then((result) => {
+
+// })
+
