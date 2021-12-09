9c8bc77d848a7bf42fc29664db7625d457e5ae2d
Konstantin Beloglazov <ajoq@ya.ru>
2021-12-09 17:14:00 +0300

    dev v3

--- src/js/__tests__/tests.js
+++ src/js/__tests__/tests.js
@@ -4,7 +4,7 @@ import GameSavingLoader from '../app';
 
 test('load return promise', () => {
     const expected = new Promise((resolve, reject) => {
-        resolve('{"id":9,"created":1546300800,"userInfo":{"id":1,name":"Hitman","level":10,"points":2000}}');
+        resolve('{"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}}');
     });
     const received = new GameSavingLoader();
     expect(received.load()).toEqual(expected);
--- src/js/app.js
+++ src/js/app.js
@@ -6,54 +6,18 @@ export default class GameSavingLoader {
         return new Promise((resolve, reject) => {
             read()
             .then((result) => {
-                return json(result);
-            })
-            .then((result) => {
-                resolve(result);
+                resolve (json(result));
             })
         })
     }
 }
 
-// GameSavingLoader.load().then((saving) => {
-//     // saving объект класса GameSaving
-//     console.log(saving);
-//   }, (error) => {
-//     // ...
-//   });
+const test1 = JSON.parse('{"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}}');
+console.log(test1);
+console.log('---');
 
-
-// const test = new GameSavingLoader();
-// console.log(test.load());
-// test.load().then((saving) => {
-//     console.log(saving);
-//     return saving;
-//   }, (error) => {
-//       console.log(error);
-//   });
-
-// let p = new Promise((resolve, reject) => {
-//     resolve('{"id":9,"created":1546300800,"userInfo":{"id":1,name":"Hitman","level":10,"points":2000}}');
-// });
-// console.log(p);
-
-// const res1 = json(read1);
-// console.log(res1);
-
-// read1.then((result) => {
-//     console.log(result);
-//     const decodeBuf = new Uint16Array(result);
-//     console.log(decodeBuf);
-//     let resultArr2 = String.fromCharCode.apply(null, decodeBuf)
-//     console.log(resultArr2);
-//     // let resultArr = '';
-//     // decodeBuf.forEach((item) => {
-//     //     resultArr += String.fromCharCode(item);
-//     // });
-//     // console.log(resultArr);
-// });
-
-// res1.then((result) => {
-
-// })
-
+// const test2 = new GameSavingLoader
+// const test3 = test2.load().then(
+//     saving => {return JSON.parse(saving)}
+// )
+// console.log(test3);
