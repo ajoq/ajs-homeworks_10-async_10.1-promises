0516cd43843196f0720378456f10e4739397e720
Konstantin Beloglazov <ajoq@ya.ru>
2021-12-09 17:44:17 +0300

    dev4

--- src/js/__tests__/tests.js
+++ src/js/__tests__/tests.js
@@ -1,11 +1,30 @@
 import read from '../reader';
 import json from '../parser';
-import GameSavingLoader from '../app';
+import GameSavingLoader from '../GameSavingLoader';
+import saveGame from '../app';
 
 test('load return promise', () => {
-    const expected = new Promise((resolve, reject) => {
-        resolve('{"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}}');
-    });
-    const received = new GameSavingLoader();
-    expect(received.load()).toEqual(expected);
+  const expected = new Promise((resolve, reject) => {
+    resolve('{"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}}');
   });
+  const received = saveGame();
+  expect(received).toEqual(expected);
+});
+
+test('reader', () => {
+  const expected = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const data = '{"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}}';
+      return ((input) => {
+        const buffer = new ArrayBuffer(input.length * 2);
+        const bufferView = new Uint16Array(buffer);
+        for (let i = 0; i < input.length; i++) {
+          bufferView[i] = input.charCodeAt(i);
+        }
+        resolve(buffer);
+      })(data);
+    }, 1000);
+  });
+  const received = read();
+  expect(received).toEqual(expected);
+});
--- src/js/app.js
+++ src/js/app.js
@@ -1,23 +1,11 @@
-import json from './parser';
+import GameSavingLoader from './GameSavingLoader';
 import read from './reader';
 
-export default class GameSavingLoader {
-    load() {
-        return new Promise((resolve, reject) => {
-            read()
-            .then((result) => {
-                resolve (json(result));
-            })
-        })
-    }
+export default function saveGame() {
+  return GameSavingLoader.load().then(
+    (saving) => JSON.parse(saving),
+    (error) => new Error(`Возникла ошибка: ${error}`),
+  );
 }
 
-const test1 = JSON.parse('{"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}}');
-console.log(test1);
-console.log('---');
-
-// const test2 = new GameSavingLoader
-// const test3 = test2.load().then(
-//     saving => {return JSON.parse(saving)}
-// )
-// console.log(test3);
+console.log(read());
