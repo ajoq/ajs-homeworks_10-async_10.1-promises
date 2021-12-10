a6eced095fc0a10fd2a333a3c95b27ea9f75315e
Konstantin Beloglazov <ajoq@ya.ru>
2021-12-10 11:03:53 +0300

    done v2

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,6 @@ import GameSavingLoader from './GameSavingLoader';
 
 export default function saveGame() {
   return GameSavingLoader.load().then(
-    (saving) => JSON.parse(saving),
-    (error) => new Error(`Возникла ошибка: ${error}`),
+    (saving) => JSON.parse(saving)
   );
 }
